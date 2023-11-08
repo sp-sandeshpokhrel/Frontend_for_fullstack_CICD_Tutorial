d91e7d50d57eeef24f3d452028128f2c8c5edbd4
Sandesh Pokhrel <sandesh.pokhrel33@gmail.com>
2023-11-08 14:19:05 +0545

    backend connect

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,23 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 export default function Home() {
+  const [data, setData] = useState<any>();
+  const [loading, setLoading] = useState(true);
+  console.log(process.env.NEXT_PUBLIC_BACKEND_URL);
+  useEffect(() => {
+    fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/sampledata`).then((res) => {
+      res.json().then((data) => {
+        setData(data);
+        setLoading(false);
+      });
+    });
+  }, []);
   return (
     <div>
-      <p>Home page HAHAHA</p>
+      <p>Your Sample Data</p>
+      <p>{loading ? "Fetching..." : data.map((dat) => <p>{dat.text}</p>)}</p>
     </div>
   );
 }
